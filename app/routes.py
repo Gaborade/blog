dbed992a07d505df027ada269363fbefd0a33b61
Gabriel <gabdanq@gmail.com>
2020-06-08 17:32:56 +0000

    mock objects set

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,22 @@
 from app import app
+from flask import render_template
 
+
+user = {'username' : 'Young padawan'}
+posts = [
+    {
+        'author': {'username': 'Han Solo'},
+        'body': 'Look at my great works and despair'
+    },
+    {
+        'author': {'username': 'Alan Moore'},
+        'body': 'The social vulgus listens'
+    }
+]
 
 @app.route('/')   # decorators can also be used as functions for callbacks
 # totally unrelated point though
 @app.route('/index')
 def index():
-    return 'hello world'
+    return render_template('index.html', title='Home', user = user, posts=posts)
+
