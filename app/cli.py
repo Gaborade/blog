d3704ee958a26ab2f3e6284a19148831370a3e83
Gabriel <gabdanq@gmail.com>
2020-08-06 16:52:36 +0000

    wrapping pybabel translation commands with click cli enhancements

---
+++ app/cli.py
@@ -0,0 +1,48 @@
+import os
+import click
+from app import app
+
+"""Using flask's native command line interface to create
+   commandline tools to speed up the workflow of translation"""
+
+# os.system runs the command in the shell of your os. 
+# when os.system runs and there is no error, it gives output of zero(0).
+# however if an error occurs, it gives an output of 1. If error occurs,
+# a runtime error is raised
+
+@app.cli.group()
+def translate():
+    """Translation and localization commands."""
+    pass
+
+
+
+@translate.command()
+@click.argument('lang')  #   to add an argument to a commandline handler
+def init(lang):
+    """Initialize a new language"""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system('pybabel init -i messages.pot -d app/translations -l ' + lang):
+        raise RuntimeError('init command failed')
+    os.remove('messages.pot')
+
+
+@translate.command()
+def update():
+    """Updates all the languages"""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system('pybabel update -i messages.pot -d app/translations'):
+        raise RuntimeError('update command failed')
+    # if everything works perfectly the messages.pot file is deleted since it is a temporary file
+    # and can be easily regenerated
+    os.remove('messages.pot')
+
+
+
+@translate.command()
+def compile():
+    """Compile all the languages into a messages.mo file"""
+    if os.system('pybabel compile -d app/translations'):
+        raise RuntimeError('compile command failed')
--- microblog.py
+++ microblog.py
@@ -1,5 +1,6 @@
-from app import app, db  # microblog.py is the main application module
+from app import app, db, cli # microblog.py is the main application module
 from app.models import User, Post
+
 
 @app.shell_context_processor  # adding some pre-imports to shell whenever flask shell is used to test out stuff
 def make_shell_context():
